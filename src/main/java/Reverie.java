c23b2d6178e9bd73188aaaff6f253ba8c4d0fa66
Leo Cheng <lefan.cheng@outlook.com>
2025-09-03 23:00:02 +0800

    Add welcome message, exit message

--- src/main/java/Reverie.java
+++ src/main/java/Reverie.java
@@ -1,5 +1,6 @@
 public class Reverie {
     public static void main(String[] args) {
+        // Reverie picture
         String[] logo = {
                 "██████╗ ███████╗██╗   ██╗███████╗██████╗ ██╗███████╗",
                 "██╔══██╗██╔════╝██║   ██║██╔════╝██╔══██╗██║██╔════╝",
@@ -9,9 +10,21 @@ public class Reverie {
                 "╚═╝  ╚═╝╚══════╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝╚═╝╚══════╝"
         };
 
-        System.out.println("Hello from\n");
+        String horizontalLine = "____________________________________________________________";
+
+        System.out.println(horizontalLine);
+        // Print Reverie picture
         for (String line : logo) {
             System.out.println(line);
         }
+
+        // Welcome message
+        System.out.println(" Hello! I'm Reverie");
+        System.out.println(" What can I do for you?");
+        System.out.println(horizontalLine);
+
+        // Exit message
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
     }
 }
