ab0ae30c251b1baf0004dfdf0523bfe6da6a259c
Leo Cheng <lefan.cheng@outlook.com>
2025-09-18 01:00:22 +0800

    Update Event feature

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,2 +1,15 @@
 public class Event extends Task {
+    protected String from;
+    protected String to;
+
+    public Event(String description, String from, String to) {
+        super(description);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String getFullStatus() {
+        return "[E]" + super.getFullStatus() + " (from: " + from + " to: " + to + ")";
+    }
 }
