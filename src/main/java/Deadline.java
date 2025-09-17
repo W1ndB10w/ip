db2b677ea4192d77d4cafba8a8bb2975269ba64d
Leo Cheng <lefan.cheng@outlook.com>
2025-09-18 00:58:57 +0800

    Update Deadline feature

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,2 +1,13 @@
 public class Deadline extends Task {
+    protected String by;
+
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    @Override
+    public String getFullStatus() {
+        return "[D]" + super.getFullStatus() + " (by: " + by + ")";
+    }
 }
