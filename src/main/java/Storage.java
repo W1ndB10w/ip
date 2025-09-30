cfe3e0aea9e2d9750533e1901e6e9a0dff0a1ac3
Leo Cheng <lefan.cheng@outlook.com>
2025-10-01 00:10:24 +0800

    Initiate file path and data directory

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.io.File;
 
 public class Storage {
+    private static final String FILE_PATH = "./data/reverie.txt";
+
+    public Storage() {
+        createDataDirectory();
+    }
+
+    private void createDataDirectory() {
+        File directory = new File("./data");
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+    }
 }
