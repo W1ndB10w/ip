fa92c1cac0e0b4d4d6068320fb0e50e688fe3e7d
Leo Cheng <lefan.cheng@outlook.com>
2025-10-02 21:38:53 +0800

    Update AddCommand.java

--- src/main/java/AddCommand.java
+++ src/main/java/AddCommand.java
@@ -1,7 +1,14 @@
-public class AddCommand {
+public class AddCommand extends Command {
     private final Task task;
 
     public AddCommand(Task task) {
         this.task = task;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws ReverieException {
+        tasks.add(task);
+        ui.showTaskAdded(task, tasks.size());
+        storage.save(tasks.getAllTasks());
     }
 }
