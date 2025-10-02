9e384021e1b726c529a71e252aced39e148d9668
Leo Cheng <lefan.cheng@outlook.com>
2025-10-03 02:54:40 +0800

    Add package declarations in ExitCommand.java

--- src/main/java/reverie/command/ExitCommand.java
+++ src/main/java/reverie/command/ExitCommand.java
@@ -1,3 +1,9 @@
+package reverie.command;
+
+import reverie.storage.Storage;
+import reverie.ui.TaskList;
+import reverie.ui.Ui;
+
 public class ExitCommand extends Command {
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
