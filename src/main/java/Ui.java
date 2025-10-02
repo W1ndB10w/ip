ebada729051794e62e65ba1325b94d4fe6007c33
Leo Cheng <lefan.cheng@outlook.com>
2025-10-02 22:55:53 +0800

    Update showFoundTasks method in Ui.java

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -86,7 +86,14 @@ public class Ui {
     }
 
     public void showFoundTasks(TaskList tasks) throws ReverieException {
-        
+        if (tasks.isEmpty()) {
+            System.out.println(" No matching tasks found!");
+        } else {
+            System.out.println(" Here are the matching tasks in your list:");
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println(" " + (i + 1) + "." + tasks.get(i).getFullStatus());
+            }
+        }
     }
 
     public void showLoadedTasks(int count) {
