640c133f2277407d13dce74fa817a8ba2d658d52
Leo Cheng <lefan.cheng@outlook.com>
2025-10-02 21:36:56 +0800

    Update ListCommand.java

--- src/main/java/ListCommand.java
+++ src/main/java/ListCommand.java
@@ -1,3 +1,6 @@
-public class ListCommand {
-    
+public class ListCommand extends Command {
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) {
+        ui.showTaskList(tasks);
+    }
 }
