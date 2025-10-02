0d48c962cb2ca43f3a8dd7977d6feac91db4f265
Leo Cheng <lefan.cheng@outlook.com>
2025-10-03 00:49:03 +0800

    Override execute method in ScheduleCommand.java

--- src/main/java/ScheduleCommand.java
+++ src/main/java/ScheduleCommand.java
@@ -3,11 +3,16 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 
-public class ScheduleCommand {
+public class ScheduleCommand extends Command {
     private final String dateString;
     private static final DateTimeFormatter INPUT_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     public ScheduleCommand(String dateString) {
         this.dateString = dateString;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws ReverieException {
+        
     }
 }
