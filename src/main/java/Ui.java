e2d840128edabcfc4d77598f4c03ee08c8b6600d
Leo Cheng <lefan.cheng@outlook.com>
2025-10-02 20:55:35 +0800

    Add constructor in Ui.java

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,3 +1,10 @@
+import java.util.Scanner;
+
 public class Ui {
-    
+    private static final String HORIZONTAL_LINE = "____________________________________________________________";
+    private final Scanner scanner;
+
+    public Ui() {
+        this.scanner = new Scanner(System.in);
+    }
 }
