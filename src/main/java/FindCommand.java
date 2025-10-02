c454fcab74af4896e2f69bb3fe3b469406cf2256
Leo Cheng <lefan.cheng@outlook.com>
2025-10-02 22:51:31 +0800

    Override execute method in FindCommand.java

--- src/main/java/FindCommand.java
+++ src/main/java/FindCommand.java
@@ -1,7 +1,17 @@
-public class FindCommand {
+public class FindCommand extends Command {
     private final String keyword;
 
     public FindCommand(String keyword) {
         this.keyword = keyword;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws ReverieException {
+        if (keyword.trim().isEmpty()) {
+            throw new ReverieException("Please specify a keyword to search for");
+        }
+
+        TaskList matchingTasks = tasks.findTasks(keyword);
+        ui.showFoundTasks(matchingTasks);
     }
 }
