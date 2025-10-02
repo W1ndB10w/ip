f194e6a5a426a289e9ab933d9f04069098a7c73e
Leo Cheng <lefan.cheng@outlook.com>
2025-10-03 02:53:22 +0800

    Add package declarations in Command.java

--- src/main/java/reverie/command/Command.java
+++ src/main/java/reverie/command/Command.java
@@ -1,3 +1,10 @@
+package reverie.command;
+
+import reverie.exception.ReverieException;
+import reverie.storage.Storage;
+import reverie.ui.TaskList;
+import reverie.ui.Ui;
+
 public abstract class Command {
     public abstract void execute(TaskList tasks, Ui ui, Storage storage) throws ReverieException;
 
