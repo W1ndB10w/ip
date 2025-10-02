0ff2c3002a8ac4d10aa74900f55aba95e1d16b39
Leo Cheng <lefan.cheng@outlook.com>
2025-10-02 21:42:22 +0800

    Add constructor in MarkCommand.java

--- src/main/java/MarkCommand.java
+++ src/main/java/MarkCommand.java
@@ -1,3 +1,9 @@
 public class MarkCommand {
-    
+    private final String arguments;
+    private final boolean isMark;
+
+    public MarkCommand(String arguments, boolean isMark) {
+        this.arguments = arguments;
+        this.isMark = isMark;
+    }
 }
