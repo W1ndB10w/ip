c27db0911f1d6503118e5c1326694ffa73eef658
Leo Cheng <lefan.cheng@outlook.com>
2025-10-02 21:12:40 +0800

    Add welcome message in Ui.java

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -7,4 +7,37 @@ public class Ui {
     public Ui() {
         this.scanner = new Scanner(System.in);
     }
+
+    private String[] getLogo() {
+        return new String[]{
+                "██████╗ ███████╗██╗   ██╗███████╗██████╗ ██╗███████╗",
+                "██╔══██╗██╔════╝██║   ██║██╔════╝██╔══██╗██║██╔════╝",
+                "██████╔╝█████╗  ██║   ██║█████╗  ██████╔╝██║█████╗  ",
+                "██╔══██╗██╔══╝  ╚██╗ ██╔╝██╔══╝  ██╔══██╗██║██╔══╝  ",
+                "██║  ██║███████╗ ╚████╔╝ ███████╗██║  ██║██║███████╗",
+                "╚═╝  ╚═╝╚══════╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝╚═╝╚══════╝"
+        };
+    }
+
+    public void showLine() {
+        System.out.print(HORIZONTAL_LINE);
+    }
+
+    public void showWelcome() {
+        // Reverie picture
+        String[] logo = getLogo();
+
+        showLine();
+        // Print Reverie picture
+        for (String line : logo) {
+            System.out.println(line);
+        }
+
+        // Welcome message
+        System.out.println(" Hello! I'm Reverie");
+        System.out.println(" What can I do for you?");
+        showLine();
+    }
+
+
 }
