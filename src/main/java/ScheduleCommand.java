6ee888a2784d125e35573b018c12fa7e83be58e8
Leo Cheng <lefan.cheng@outlook.com>
2025-10-03 00:47:14 +0800

    Add constructor in ScheduleCommand.java

--- src/main/java/ScheduleCommand.java
+++ src/main/java/ScheduleCommand.java
@@ -1,3 +1,13 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+
 public class ScheduleCommand {
-    
+    private final String dateString;
+    private static final DateTimeFormatter INPUT_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    public ScheduleCommand(String dateString) {
+        this.dateString = dateString;
+    }
 }
