e3e97e82b4973efeab7c6422d6c7f48c5f757fd7
Leo Cheng <lefan.cheng@outlook.com>
2025-10-03 02:53:50 +0800

    Add package declarations in AddCommand.java

--- src/main/java/reverie/command/AddCommand.java
+++ src/main/java/reverie/command/AddCommand.java
@@ -1,3 +1,11 @@
+package reverie.command;
+
+import reverie.exception.ReverieException;
+import reverie.storage.Storage;
+import reverie.task.Task;
+import reverie.ui.TaskList;
+import reverie.ui.Ui;
+
 public class AddCommand extends Command {
     private final Task task;
 
