bf2f69580c6cdd6dba680ecf66b18c1741ff1e48
Leo Cheng <lefan.cheng@outlook.com>
2025-10-02 21:35:41 +0800

    Update ExitCommand.java

--- src/main/java/ExitCommand.java
+++ src/main/java/ExitCommand.java
@@ -1,3 +1,11 @@
-public class ExitCommand {
-    
+public class ExitCommand extends Command {
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) {
+        ui.showGoodbye();
+    }
+
+    @Override
+    public boolean isExit() {
+        return true;
+    }
 }
