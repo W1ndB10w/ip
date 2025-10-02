1000598760b21179b84b42f60a65e8ffc3acbde0
Leo Cheng <lefan.cheng@outlook.com>
2025-10-02 21:22:42 +0800

    Add constructors in TaskList.java

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,3 +1,13 @@
+import java.util.ArrayList;
+
 public class TaskList {
-    
+    private final ArrayList<Task> tasks;
+
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
 }
