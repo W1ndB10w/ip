7ff23fb0b0ffe5d6ccc8ff4e465342ff14079b86
Leo Cheng <lefan.cheng@outlook.com>
2025-10-02 21:53:45 +0800

    Identify different command types in Parser.java

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -9,5 +9,26 @@ public class Parser {
         String[] parts = trimmedCommand.split("\\s+", 2);
         String commandWord = parts[0].toLowerCase();
         String arguments = parts.length > 1 ? parts[1] : "";
+
+        switch (commandWord) {
+            case "bye":
+                return new ExitCommand();
+            case "list":
+                return new ListCommand();
+            case "mark":
+                return new MarkCommand(arguments, true);
+            case "unmark":
+                return new MarkCommand(arguments, false);
+            case "todo":
+                return new AddCommand(parseTodo(trimmedCommand));
+            case "deadline":
+                return new AddCommand(parseDeadline(trimmedCommand));
+            case "event":
+                return new AddCommand(parseEvent(trimmedCommand));
+            case "delete":
+                return new DeleteCommand(arguments);
+            default:
+                throw new ReverieException("I'm sorry, but I don't know what that means :-(");
+        }
     }
 }
