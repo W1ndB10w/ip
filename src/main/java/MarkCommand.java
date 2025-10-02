06ea58de6324ecae4ee7f259a43cdf1bce6a5920
Leo Cheng <lefan.cheng@outlook.com>
2025-10-02 21:43:15 +0800

    Override execute method in MarkCommand.java

--- src/main/java/MarkCommand.java
+++ src/main/java/MarkCommand.java
@@ -1,9 +1,14 @@
-public class MarkCommand {
+public class MarkCommand extends Command {
     private final String arguments;
     private final boolean isMark;
 
     public MarkCommand(String arguments, boolean isMark) {
         this.arguments = arguments;
         this.isMark = isMark;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws ReverieException {
+        
     }
 }
