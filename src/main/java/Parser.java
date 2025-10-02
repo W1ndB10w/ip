293853dc120e2804919fef918e4bf9e54be2fac8
Leo Cheng <lefan.cheng@outlook.com>
2025-10-02 21:58:36 +0800

    Modify 'switch' expression in Parser.java

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -10,26 +10,17 @@ public class Parser {
         String commandWord = parts[0].toLowerCase();
         String arguments = parts.length > 1 ? parts[1] : "";
 
-        switch (commandWord) {
-            case "bye":
-                return new ExitCommand();
-            case "list":
-                return new ListCommand();
-            case "mark":
-                return new MarkCommand(arguments, true);
-            case "unmark":
-                return new MarkCommand(arguments, false);
-            case "todo":
-                return new AddCommand(parseTodo(trimmedCommand));
-            case "deadline":
-                return new AddCommand(parseDeadline(trimmedCommand));
-            case "event":
-                return new AddCommand(parseEvent(trimmedCommand));
-            case "delete":
-                return new DeleteCommand(arguments);
-            default:
-                throw new ReverieException("I'm sorry, but I don't know what that means :-(");
-        }
+        return switch (commandWord) {
+            case "bye" -> new ExitCommand();
+            case "list" -> new ListCommand();
+            case "mark" -> new MarkCommand(arguments, true);
+            case "unmark" -> new MarkCommand(arguments, false);
+            case "todo" -> new AddCommand(parseTodo(trimmedCommand));
+            case "deadline" -> new AddCommand(parseDeadline(trimmedCommand));
+            case "event" -> new AddCommand(parseEvent(trimmedCommand));
+            case "delete" -> new DeleteCommand(arguments);
+            default -> throw new ReverieException("I'm sorry, but I don't know what that means :-(");
+        };
     }
 
     private static Task parseTodo(String input) throws ReverieException {
