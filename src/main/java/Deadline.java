c4e053795d08aab2a37f5e55a46abd28272c4e41
Leo Cheng <lefan.cheng@outlook.com>
2025-10-03 00:22:39 +0800

    Add date and time variables in Deadline.java

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,5 +1,8 @@
 public class Deadline extends Task {
     protected String by;
+    protected LocalDate byDate;
+    private static final DateTimeFormatter INPUT_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private static final DateTimeFormatter OUTPUT_FORMAT = DateTimeFormatter.ofPattern("MMM d yyyy");
 
     public Deadline(String description, String by) {
         super(description);
