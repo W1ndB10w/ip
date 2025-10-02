e3bbb2f2d05bee919bafaf390a60f5bfc8102d5f
Leo Cheng <lefan.cheng@outlook.com>
2025-10-03 00:23:23 +0800

    Modify constructor in Deadline.java

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -7,6 +7,7 @@ public class Deadline extends Task {
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
+        this.byDate = parseDate(by);
     }
 
     @Override
