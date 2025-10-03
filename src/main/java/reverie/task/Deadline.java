ca10782abd7b6867ef1aca0e96e668c7de046e9b
Leo Cheng <lefan.cheng@outlook.com>
2025-10-03 10:23:11 +0800

    Update imports in Deadline.java

--- src/main/java/reverie/task/Deadline.java
+++ src/main/java/reverie/task/Deadline.java
@@ -1,9 +1,8 @@
 package reverie.task;
 
 import reverie.exception.ReverieException;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 import java.util.Locale;
 
 public class Deadline extends Task {
