f7192765346e3417f87c9f879736e0de1f25b979
Leo Cheng <lefan.cheng@outlook.com>
2025-10-03 10:44:07 +0800

    Add instance variables in DateTimeParser.java

--- src/main/java/reverie/task/DateTimeParser.java
+++ src/main/java/reverie/task/DateTimeParser.java
@@ -1,5 +1,7 @@
 package reverie.task;
 
 public class DateTimeParser {
-    
+    private static final List<DateTimeFormatter> DATE_TIME_FORMATTERS = new ArrayList<>();
+    private static final List<DateTimeFormatter> DATE_FORMATTERS = new ArrayList<>();
+    private static final List<DateTimeFormatter> TIME_FORMATTERS = new ArrayList<>();
 }
